5ee56f6936edce60b049647dcae17da4602e5754
Ben Smerd <bsmerd@adam.com.au>
2022-11-18 17:49:54 +1030

    criteria filtering for empty inputs for fetch call

--- client/src/hooks/IMDBAPI.tsx
+++ client/src/hooks/IMDBAPI.tsx
@@ -10,11 +10,16 @@ const getData = async (criteria: Criteria) => {
     }
 
     //TODO typeEntertainemt = title_type=feature,tv_series
+    if (criteria.typeEntertainment === "Movie") {
+      criteria.typeEntertainment = "feature";
+    } else if (criteria.typeEntertainment === "TV Series") {
+      criteria.typeEntertainment = "tv_series";
+    }
 
     //TODO user rating will have to play around with the two nums in the url below, might ust mean 6 out of 10 or 6 to 10
     console.log(criteria);
     const { data } = await axios.get(
-      `https://imdb-api.com/API/AdvancedSearch/k_q5dx85dn?title=${criteria.title}&user_rating=6,10&release_date=2010-01-01,2022-01-01&genres=${criteria.genre}`
+      `https://imdb-api.com/API/AdvancedSearch/k_q5dx85dn?title=${criteria.title}&user_rating=${criteria.rating},10&title_type=${criteria.typeEntertainment}&genres=${criteria.genre}`
     );
 
     const listResponse = await data.results;
