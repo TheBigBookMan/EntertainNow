55fa33a7a930dd5b7c7249d0ea5da227e0737e63
Ben Smerd <bsmerd@adam.com.au>
2022-11-19 15:58:14 +1030

    added in styling for favourite list item

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -1,18 +1,45 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Footer = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
+
+  //!! HAVE TEMPORARY SIGN IN SIGN OUT CLICK SETSTATE
   return (
     <div className="flex justify-between items-center p-2 pt-3">
-      <Link to="/signin">
-        <button className="border-solid border-2 bg-zinc-200 rounded-lg w-[100px] h-[40px] hover:bg-zinc-300">
-          Sign In
-        </button>
-      </Link>
-      <Link to="/signup">
-        <button className="border-solid border-2 bg-zinc-200 rounded-lg w-[100px] h-[40px] hover:bg-zinc-300">
-          Sign Up
-        </button>
-      </Link>
+      {isLoggedIn ? (
+        <>
+          <Link to="/">
+            <button
+              onClick={() => setIsLoggedIn(false)}
+              className="border-solid border-2 bg-zinc-200 rounded-lg w-[100px] h-[40px] hover:bg-zinc-300"
+            >
+              Sign Out
+            </button>
+          </Link>
+          <Link to="/favourites">
+            <button className="border-solid border-2 bg-zinc-200 rounded-lg w-[100px] h-[40px] hover:bg-zinc-300">
+              Favourites
+            </button>
+          </Link>
+        </>
+      ) : (
+        <>
+          <Link to="/signin">
+            <button
+              onClick={() => setIsLoggedIn(true)}
+              className="border-solid border-2 bg-zinc-200 rounded-lg w-[100px] h-[40px] hover:bg-zinc-300"
+            >
+              Sign In
+            </button>
+          </Link>
+          <Link to="/signup">
+            <button className="border-solid border-2 bg-zinc-200 rounded-lg w-[100px] h-[40px] hover:bg-zinc-300">
+              Sign Up
+            </button>
+          </Link>
+        </>
+      )}
     </div>
   );
 };
--- client/src/pages/Favourites.tsx
+++ client/src/pages/Favourites.tsx
@@ -1,9 +1,28 @@
 import Container from "../components/common/Container";
+import { BsSuitHeartFill } from "react-icons/bs";
 
 const Favourites = () => {
+  // TODO add star react icon for rating
+
+  // TODO add heart icon
   return (
     <Container>
-      <h1>Favourites</h1>
+      <h1 className="font-bold text-2xl text-center">Favourites</h1>
+      <ul className="flex flex-col gap-2">
+        <li className="flex justify-between border-b-2">
+          <div className="flex flex-col">
+            <div className="flex flex-row gap-2 items-center">
+              <BsSuitHeartFill className="text-md hover:cursor-pointer hover:text-lg" />
+              <h1 className="font-bold">Inception</h1>
+              <p>2022</p>
+            </div>
+            <p className="text-sm">8 STAR</p>
+            <p className="text-sm">PG rated</p>
+          </div>
+
+          <p>image</p>
+        </li>
+      </ul>
     </Container>
   );
 };
