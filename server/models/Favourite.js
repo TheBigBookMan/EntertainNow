3294540be5c27d917f8943570620bb62870d14f7
Ben Smerd <bsmerd@adam.com.au>
2022-11-19 21:40:20 +1030

    created favourite model schema and properties

--- server/models/Favourite.js
+++ server/models/Favourite.js
@@ -1,1 +1,30 @@
-// ! Poperties- title, year, description, youtube link, actors, directors, rating, genre
+const { Schema, model } = require("mongoose");
+
+// ! Poperties- title, description, imdbRating, contentrating, image
+
+const favouriteSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imDbRating: {
+    type: String,
+    required: true,
+  },
+  contentRating: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+const Favourite = model("Favourite", favouriteSchema);
+
+module.exports = Favourite;
