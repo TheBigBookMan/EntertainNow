4cb8c34e92495c4970d9a18ac80378c3b3a9c0fb
Ben Smerd <bsmerd@adam.com.au>
2022-11-20 13:07:56 +1030

    added in password hashing with bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -48,6 +48,15 @@ userSchema.virtual("favouriteCount").get(function () {
   return this.favourites.length;
 });
 
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
 userSchema.methods.isCorrectPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
