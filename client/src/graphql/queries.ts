6d089b3cf9c8cc3308983084570002b39c8de34f
Ben Smerd <bsmerd@adam.com.au>
2022-11-21 18:45:00 +1030

    added get favourites query in client

---
+++ client/src/graphql/queries.ts
@@ -0,0 +1,60 @@
+import { gql } from "@apollo/client";
+
+export const GET_FAVOURITES = gql`
+  query GetFavourites($userId: ID!) {
+    favourites(userId: $userId) {
+      _id
+      title
+      description
+      imDbRating
+      contentRating
+      image
+      youtube
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation AddUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+      user {
+        _id
+        username
+        password
+        email
+        favourites {
+          _id
+          title
+          description
+          imDbRating
+          contentRating
+          image
+          youtube
+        }
+      }
+      token
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation Login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      user {
+        _id
+        username
+        password
+        email
+        favourites {
+          _id
+          title
+          description
+          imDbRating
+          contentRating
+          image
+          youtube
+        }
+      }
+    }
+  }
+`;
