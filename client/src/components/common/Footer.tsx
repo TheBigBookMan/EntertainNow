5bc302ebfeb2c6192b048873340da82ae5cac238
Ben Smerd <bsmerd@adam.com.au>
2022-11-25 17:47:23 +1030

    update footer functionality with user context for logout

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import UseUserContext from "../../contexts/UserContext";
 
 const Footer = () => {
-  const { isLoggedIn } = UseUserContext();
-  //!! HAVE TEMPORARY SIGN IN SIGN OUT CLICK SETSTATE
-  console.log(isLoggedIn);
+  const { isLoggedIn, logoutUser } = UseUserContext();
   return (
     <div className="flex justify-between items-center p-2 pt-3">
       {isLoggedIn ? (
         <>
           <Link to="/">
-            <button className="border-solid border-2 bg-zinc-200 rounded-lg w-[100px] h-[40px] hover:bg-zinc-300">
+            <button
+              onClick={logoutUser}
+              className="border-solid border-2 bg-zinc-200 rounded-lg w-[100px] h-[40px] hover:bg-zinc-300"
+            >
               Sign Out
             </button>
           </Link>
