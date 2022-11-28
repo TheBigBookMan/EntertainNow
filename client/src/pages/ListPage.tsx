dbc4883a25f35c97f740853823185f319534fb9f
Ben Smerd <bsmerd@adam.com.au>
2022-11-28 17:13:34 +1030

    added in conditional rendering for adding to favourites if user is logged in

--- client/src/pages/ListPage.tsx
+++ client/src/pages/ListPage.tsx
@@ -6,11 +6,13 @@ import Container from "../components/common/Container";
 import { ADD_FAVOURITE, REMOVE_FAVOURITE } from "../graphql/queries";
 import { useMutation } from "@apollo/client";
 import getYoutube from "../hooks/YoutubeAPI";
+import useCtx from "../contexts/UserContext";
 
 //! FIX THE ANYS
 const ListPage = ({ criteria }: any) => {
   const [movieList, setMovieList] = useState<MovieProps[]>([]);
   const [addFavourite, { error }] = useMutation(ADD_FAVOURITE);
+  const { isLoggedIn } = useCtx();
 
   // TODO have the add favourite and remove favourite mutation
 
@@ -24,10 +26,6 @@ const ListPage = ({ criteria }: any) => {
   useEffect(() => {
     makeAPICall();
   }, []);
-
-  {
-    /* PUT IN A CANT USE COMONENET FOR IF USER IS LOGGED IN*/
-  }
 
   const fetchYoutube = async (title: string, year: string) => {
     const youtubeUrl = await getYoutube(title, year);
@@ -70,10 +68,12 @@ const ListPage = ({ criteria }: any) => {
               <div className="flex items-center gap-2">
                 <h1 className="font-bold text-xl">{movie.title}</h1>
                 <p className="italic">{movie.description}</p>
-                <BsSuitHeart
-                  onClick={() => addToFavourite(movie)}
-                  className="hover:cursor-pointer hover:text-lg"
-                />
+                {isLoggedIn && (
+                  <BsSuitHeart
+                    onClick={() => addToFavourite(movie)}
+                    className="hover:cursor-pointer hover:text-lg"
+                  />
+                )}
               </div>
               <div className="flex gap-5">
                 <p className="flex items-center">
