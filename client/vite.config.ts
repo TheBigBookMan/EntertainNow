0ba1345290c7751b18be7c058e9d40bcebc15941
Ben Smerd <bsmerd@adam.com.au>
2022-12-18 20:23:26 +1030

    fix bug

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // dev server on 8090
-    proxy: {
-      "/signin/": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-      },
-    },
-  },
 });
