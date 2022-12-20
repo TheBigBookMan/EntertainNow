89fa3eb92ee3683f0a4541d057421747ec6b8b40
Ben Smerd <bsmerd@adam.com.au>
2022-12-20 15:20:06 +1030

    change fongid server

--- server/index.js
+++ server/index.js
@@ -16,11 +16,11 @@ app.use(express.json());
 // app.use(require("cookie-parser")());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 }
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 // * Start the Apollo server connection with middleware
