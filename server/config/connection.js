5bc6dd91ba96302ccb0e1225f2dbaa2469cfffd1
Ben Smerd <bsmerd@adam.com.au>
2022-12-20 13:36:10 +1030

    bug fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
+// mongodb+srv://bsmerd:Thisisthe!0211@cluster0.j1mnute.mongodb.net/?retryWrites=true&w=majority
+
 // * Create connection to the MongoDB database
 mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://bsmerd:Thisisthe!0211@cluster0.j1mnute.mongodb.net/?retryWrites=true&w=majority",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/entertainnow",
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
