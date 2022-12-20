32719c3ce4d08f0e4ac7383c06f3c1fc7e1bd540
Ben Smerd <bsmerd@adam.com.au>
2022-12-20 20:24:05 +1030

    fix server

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 }
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
