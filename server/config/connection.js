036bb9db9f2590258916cf48bedc872850dc87e3
Ben Smerd <bsmerd@adam.com.au>
2022-12-20 13:48:29 +1030

    bug

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 // * Create connection to the MongoDB database
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/entertainnow",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/entertainnow",
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
